a4d286028647529fc9b68ad5de1b030346feab68
christopherkeibel <christopher.keibel@karakun.com>
2023-09-10 11:01:52 +0200

    use all api points to fetch data

--- 2-Semester/Databases-for-BigData/service/datamodel.py
+++ 2-Semester/Databases-for-BigData/service/datamodel.py
@@ -1,20 +1,50 @@
-from neomodel import (StructuredNode, 
+from neomodel import (StructuredNode,
+                      StructuredRel, 
                       RelationshipTo, 
                       RelationshipFrom, 
                       StringProperty, 
-                      BooleanProperty, 
-                      ArrayProperty,
+                      BooleanProperty,
+                      FloatProperty,
                       DateTimeProperty,
                       IntegerProperty)
 
 class Tag(StructuredNode):
     # address or name as 'id'
     address = StringProperty(unique_index=True, required=True)
-    sensors = ArrayProperty()
     name = StringProperty()
     last_contact = StringProperty() # TODO: date
     online = BooleanProperty
     gateway = RelationshipFrom('Gateway', 'CONNECTED_TO')
+    accleration_sensor = RelationshipTo('AcclerationSensor', 'CONNECTED_TO')
+    humidity_sensor = RelationshipTo('HumiditySensor', 'CONNECTED_TO')
+    voltage_sensor = RelationshipTo('VoltageSensor', 'CONNECTED_TO')
+    temperature_sensor = RelationshipTo('TemperatureSensor', 'CONNECTED_TO')
+
+
+# Maybe rel
+class GatewayTagConfig(StructuredRel):
+    samplerate = IntegerProperty()
+    scan_interval = IntegerProperty()
+    resolution = IntegerProperty()
+    scale = IntegerProperty()
+    dsp_function = IntegerProperty()
+    dsp_parameter = IntegerProperty()
+    mode = IntegerProperty()
+    divider = IntegerProperty()
+
+    def to_dict(self) -> dict:
+        mapping = {
+            "samplerate": self.samplerate,
+            "scan_interval": self.scan_interval,
+            "resolution": self.resolution,
+            "scale": self.scale,
+            "dsp_function": self.dsp_function,
+            "dsp_parameter": self.dsp_parameter,
+            "mode": self.mode,
+            "divider": self.divider
+        }
+        return mapping
+
 
 class Gateway(StructuredNode):
     gid = StringProperty(unique_index=True, required=True)
@@ -22,4 +52,38 @@ class Gateway(StructuredNode):
     last_contact =  StringProperty() # TODO: date
     online = BooleanProperty()
     ip_address = StringProperty()
-    tag = RelationshipTo('Tag', 'CONNECTED_TO') # cardiality=ZeroOrMore (default)
+    tag = RelationshipTo('Tag', 'CONFIGURES', model=GatewayTagConfig) # cardiality=ZeroOrMore (default)
+    config = RelationshipTo('Config', 'STORES')
+
+
+class Config(StructuredNode):
+    version = StringProperty()
+    poll_interval = IntegerProperty()
+    max_allowed_clients = IntegerProperty()
+    api_timeout = IntegerProperty()
+
+
+class AcclerationSensor(StructuredNode):
+    acc_x = FloatProperty()
+    acc_y = FloatProperty()
+    acc_z = FloatProperty()
+    movement_counter = IntegerProperty()
+    recorded_time = StringProperty()
+
+
+class HumiditySensor(StructuredNode):
+    humidity = FloatProperty()
+    humidity_scale = IntegerProperty()
+    recorded_time = StringProperty()
+
+
+class VoltageSensor(StructuredNode):
+    voltage = FloatProperty()
+    voltage_scale = IntegerProperty()
+    recorded_time = StringProperty()
+
+
+class TemperatureSensor(StructuredNode):
+    temperature = FloatProperty()
+    temperature_scale = IntegerProperty()
+    recorded_time = StringProperty()
--- 2-Semester/Databases-for-BigData/service/service.py
+++ 2-Semester/Databases-for-BigData/service/service.py
@@ -1,7 +1,100 @@
-from datamodel import Gateway, Tag
+from datamodel import (Gateway,
+                        Tag, 
+                        Config, 
+                        GatewayTagConfig,
+                        AcclerationSensor,
+                        TemperatureSensor,
+                        HumiditySensor,
+                        VoltageSensor
+                        )
 from neomodel import config
 import requests
 import json
+import logging
+
+# logging.basicConfig(filename='logging.log', encoding='utf-8', level=logging.WARNING)
+
+
+# status code decorator
+def check_status_code(func):
+    def inner(api):
+        logger = logging.getLogger(__name__)
+        status_code = requests.get(api).status_code
+        if  status_code != 200:
+            logger.warning(f"Bad request: {status_code}")
+            return None
+        
+        return func(api)
+    
+    return inner
+
+
+@check_status_code
+def get_config(api:str) -> Config | None:
+    config_api = requests.get(api)
+    c = json.loads(config_api.text)["config"]
+    cfg = Config(
+        version=c["version"],
+        poll_interval=c["poll_interval"],
+        max_allowed_clients=c["max_allowed_clients"],
+        api_timeout=c["api_timeout"]
+    ).save()
+    return cfg  
+
+
+@check_status_code
+def get_gateway_tag_config(api: str) -> GatewayTagConfig:
+    config_api = requests.get(api)
+    c: dict = json.loads(config_api.text)["config"]
+    cfg = GatewayTagConfig(
+        samplerate=c["samplerate"],
+        scan_interval=c["scan_interval"],
+        resolution=c["resolution"],
+        scale=c["scale"],
+        dsp_function=c["dsp_function"],
+        dsp_parameter=c["dsp_parameter"],
+        mode=c["mode"],
+        divider=c["divider"]
+    )
+
+    return cfg
+
+
+@check_status_code
+def get_sensor_data(api: str) -> tuple[AcclerationSensor, TemperatureSensor, HumiditySensor, VoltageSensor]:
+    sensor_api = requests.get(api)
+    try:
+        s: list[dict] = json.loads(sensor_api.text)["measurements"]
+    except json.JSONDecodeError:
+        return None
+
+    acc = AcclerationSensor(
+        acc_x=s[0]["acc_x"],
+        acc_y=s[0]["acc_y"],
+        acc_z=s[0]["acc_z"],
+        movement_counter=s[0]["movement_counter"],
+        recorded_time=s[0]["recorded_time"]
+    ).save()
+
+    temp = TemperatureSensor(
+        temperature=s[1]["temperature"],
+        temperature_scale=s[1]["temperature_scale"],
+        recorded_time=s[1]["recorded_time"]
+    ).save()
+
+    hum = HumiditySensor(
+        humidity=s[2]["humidity"],
+        humidity_scale=s[2]["humidity_scale"],
+        recorded_time=s[2]["recorded_time"]
+    ).save()
+
+    vol = VoltageSensor(
+        voltage=s[3]["voltage"],
+        voltage_scale=s[3]["voltage_scale"],
+        recorded_time=s[3]["recorded_time"]
+    ).save()
+
+    return (acc, temp, hum, vol)
 
 
 def fetch_and_persist() -> None:
@@ -24,6 +117,12 @@ def fetch_and_persist() -> None:
                     online=g["online"],
                     ip_address=g["ip_address"]
                 ).save()
+
+            # Get gateway config
+            cfg = get_config(f"http://localhost:8080/api/v1/config/get/{gateway.gid}")
+            if cfg:
+                gateway.config.connect(cfg)
+
             
             # Fetch tags for gateway
             tag_api = requests.get(f"http://localhost:8080/api/v1/structure/tag/list/{g['id']}")
@@ -34,14 +133,25 @@ def fetch_and_persist() -> None:
                         # Add tag to gateway tag-list
                         tag = Tag(
                             address=t["address"],
-                            sensors=t["sensors"],
                             name=t["name"],
                             last_contact=t["last_contact"],
                             online=t["online"],
                         ).save()
 
-                    gateway.tag.connect(tag)
+                    # Get gateway to tag relationship properties
+                    gateway_tag = get_gateway_tag_config(f"http://localhost:8080/api/v1/config/get/{gateway.gid}/{tag.address}")
+                    gateway.tag.connect(tag, gateway_tag.to_dict())
 
+                    # Get tag sensor data
+                    acc, temp, hum, vol = get_sensor_data(f"http://localhost:8080/api/v1/acc-data/get/{tag.address}") or (None, None, None, None)
+                    if acc:
+                        tag.accleration_sensor.connect(acc)
+                    if temp:
+                        tag.temperature_sensor.connect(temp)
+                    if hum:
+                        tag.humidity_sensor.connect(hum)
+                    if vol:
+                        tag.voltage_sensor.connect(vol)
             
 
 
